0865da86162747c85d52b5063e9145f1c049b808
Jason Wolfe <Jasonwolfe@gmail.com>
2017-09-06 23:02:12 -0700

    stopped calling register service worker

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,9 +19,9 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="container">
         <BrowserRouter>
-          <div className="container">
+          <div>
             <Header />
             <Switch>
               <Route path="/surveys/new" component={SurveyNew} />
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import reduxThunk from "redux-thunk";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
@@ -16,4 +16,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
