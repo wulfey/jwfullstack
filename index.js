543b1d8d45c0b6a7fded06619ba5a199a8811b3a
Jason Wolfe <Jasonwolfe@gmail.com>
2017-09-06 22:19:07 -0700

    json wrong?no, said BUILT

--- index.js
+++ index.js
@@ -31,9 +31,12 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  // Express will server production assets, main.js, main.css
-  app.use(express.static("client/built"));
-  // Express will serve up the index.html if not recognized
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
